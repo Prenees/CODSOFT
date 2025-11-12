862e11e511f10bf994fd9280b646a4608584b347
Prenees <pulsarprenees@gmail.com>
2025-11-12 20:19:01 +0530

    modified

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,34 +1,61 @@
 import java.util.Scanner;
-
 class StudentGradeCalculator
 {
     public static void main(String[]args)
     {
         Scanner scn=new Scanner(System.in);
-        System.out.println("Student Grade Calculator");
-        System.out.println("Enter how many subjects");
-        int num=scn.nextInt();
-
-        int[]marks=new int[num];
+        System.out.println("---Student Grade Calculator----");
+        System.out.println("Enter how many subject");
+        int numofsub=scn.nextInt();
+        
+        // Stored marks
+        int[] marks=new int[numofsub];
         int total=0;
 
-        for(int i=0;i < num; i++)
+        for(int i=0;i<numofsub;i++)
         {
-            System.out.println("Enter your marks "+(i+1)+" out of(100)");
-             int mark =scn.nextInt();
+            System.out.println("Enter your mark"+(i+1)+"out of(100)");
+            int mark=scn.nextInt();
 
             if(mark<0 || mark>100)
             {
-                System.out.println("! invalid marks..pls enter valid marks out of 0 to 100");
+                System.out.println("Invalid mark..pls enter mark out of 0 to 100");
                 i--;
                 continue;
             }
             marks[i]=mark;
-            total+=mark;
+            total=total+mark;
         }
-        // average
-        double avg=total/num;
+        // Calculate average
+        double avg=total/numofsub;
+
+        // Grade Display
+        char grade;
+        if(avg>=90)
+        {
+            grade='A';
+        }
+        else if(avg>=80)
+        {
+            grade='B';
+        }
+        else if(avg>=70)
+        {
+            grade='C';
+        }
+        else if(avg>=60)
+        {
+            grade='D';
+        }
+        else
+        {
+            grade='F';
+        }
+
+        //Result
+        System.out.println("-----Result Display------");
         System.out.println("Total marks ="+total);
-        System.out.println("Average ="+avg);
+        System.out.println("Average  ="+avg);
+        System.out.println("Grade  ="+grade);
     }
 }
